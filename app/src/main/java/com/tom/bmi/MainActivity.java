319cbc67e45d059b939c9250c05a28aa2f35914e
Hank Tom <hanktom@gmail.com>
2018-10-02 16:08:28 +0800

    findViewById, get data , bmi

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.tom.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bmi(View view) {
+        Log.d("MainActivity", "bmi");
+        EditText edWeight = findViewById(R.id.ed_weight);
+        EditText edHeight = findViewById(R.id.ed_height);
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        Log.d("MainActivity", w + "/" + h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", bmi + "");
     }
 }
