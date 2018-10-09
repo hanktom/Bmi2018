b3734c3562114779b2b2f9cd5389cedfdbacfcbb
Hank Tom <hanktom@gmail.com>
2018-10-09 16:20:06 +0800

    10/9 Refactor, Toast, AlertDialog

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,23 +1,29 @@
 package com.tom.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
     }
 
     public void bmi(View view) {
         Log.d("MainActivity", "bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         Log.d("MainActivity", w + "/" + h);
@@ -25,5 +31,19 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity", bmi + "");
+        Toast.makeText(this, "Your BMI is " + bmi,
+                Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is " + bmi)
+                .setTitle("BMI")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        edWeight.setText("");
+                        edHeight.setText("");
+                    }
+                })
+                .show();
     }
 }
